7638fb4747328ee7901603a7bc7c9f302d8b83e4
Davi Silva do Prado <1221316629@acd.grupotiradentes.com>
2023-11-10 10:02:41 -0300

    chore: add GitHub Actions workflow para integração contínua

--- calculadora.test.js
+++ calculadora.test.js
@@ -4,6 +4,7 @@ describe('Teste da função somar', () => {
     test('Deveria somar 2 numeros inteiros positivos', () => {
         expect(somar(2,3)).toBe(5) 
         expect(somar(1,0)).toBe(1) 
+        expect(somar(10,0)).toBe(10) 
     })
     test('Deveria somar 2 numeros inteiros negativos', () => {
         expect(somar(-1,-1)).toBe(-2)
